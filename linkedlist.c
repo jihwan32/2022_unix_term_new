60b6f97a3f32c70618f71fb8b1b35f261af1b81d
jihwan <pgihwan32@naver.com>
2022-05-18 11:08:41 +0900

    node header, linkedlist

---
+++ linkedlist.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include "node.h"
+
+static Node* _head = NULL;
+static Node* _tail = NULL;
+static Node* _cur_node = NULL;
+
+
+bool empty(){
+
+	return ;
+}
+
+size_t size(){
+
+	return ;
+}
+
+void print(){
+
+	return ;
+}
+
+void print_file(FILE* stream){
+
+	return ;
+}
+
+void clear(){
+
+	return ;
+}
+
+Node* append_left(size_t n, char new_data[n]){
+
+	return ;
+}
+
+Node* insert_after(Node* cur_node, Node* new_node){
+
+	return ;
+}
+
+Node* append(size_t n, char new_data[n]){
+
+	return ;
+}
+
+Node* delete_node(Node* cur_node){
+
+	return ;
+}
+
+Node* delete(char* data){
+
+	return ;
+}
+
+Node* get_node(size_t index){
+
+	return ;
+}
+
+Node* first(){
+
+	return ;
+}
+
+Node* last(){
+
+	return ;
+}
+
+Node* next(){
+
+	return ;
+}
+
+Node* prev(){
+
+	return ;
+}
+
